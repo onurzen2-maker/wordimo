033f2688ed7fc24b37960af329a6cb830e55d6c1
Onur Ozen <onurzen2@gmail.com>
2026-08-27 15:40:59 +0300

    Sitemap domain wordimoacademy olarak duzeltildi

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.wordimoakademi.com',
+      url: 'https://www.wordimoacademy.com',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 1.0,
